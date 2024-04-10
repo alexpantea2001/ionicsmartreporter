a5af12ae00c0fdd5a9b74ec3a82af4bc0076c617
Alex Ramon Pantea <cti20b109@cti.uoradea>
2024-04-10 19:18:58 +0300

    My first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ro.uoradea.cti.padmw.ionic.smartreporter',
+  appName: 'IonicSmartReporter',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
